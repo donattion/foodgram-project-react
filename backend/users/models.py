93903b3f6ddba726b629a4f0f15eca14bb95c597
donattion <donat.medvedev@yandex.ru>
2022-12-23 23:51:16 +0300

    v0.0.6

--- backend/users/models.py
+++ backend/users/models.py
@@ -35,7 +35,7 @@ class Manager(UserManager):
     def create_user(self, email, password=None):
         if not email:
             raise ValueError('Пользователь должен иметь email')
-        user=self.model(email=email,)
+        user = self.model(email=email,)
         user.set_password(password)
         user.save(using=self._db)
         return user

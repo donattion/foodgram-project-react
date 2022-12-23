d36b820767083ba9c465b8d9b7b5f40be59a789d
donattion <donat.medvedev@yandex.ru>
2022-12-23 23:49:46 +0300

    v0.0.5

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 
 
@@ -29,3 +29,13 @@ class User(AbstractUser):
 
     def __str__(self) -> str:
         return self.username
+
+
+class Manager(UserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('Пользователь должен иметь email')
+        user=self.model(email=email,)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
